98deae8a8be11d8dd8135feab56091596836843a
Bekican <164552682+Bekican@users.noreply.github.com>
2024-08-27 19:09:58 +0300

    first project

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require("./models/product.model.js")
+const productRoute = require("./routes/product.route.js")
+const app = express()
+
+
+
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//routes
+app.use("/api/products",productRoute)
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node API Server Updated');
+});
+
+
+
+
+
+
+mongoose.connect("mongodb+srv://bekircancakmak:IzURtUSGL2kpM55d@firstbackend.hsjsn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=FirstBackend")
+    .then(() => {
+        console.log("Connected to Mongo Db");
+        app.listen(8000, () => {
+            console.log("Server is running on port 8000");
+        })
+
+    })
+    .catch(() => {
+        console.log("Connection Failed")
+    })
